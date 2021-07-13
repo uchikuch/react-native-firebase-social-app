a0a3f15b5240d71d97dc0814317b2d357858a28d
uchikuch <ucheazinge@gmail.com>
2021-07-13 16:19:07 +0100

    Begin Login & Signup UI

---
+++ navigation/AuthStack.js
@@ -0,0 +1,52 @@
+import React, {useEffect} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import OnboardingScreen from '../screens/OnboardingScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+  let routeName;
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch == null) {
+    return null;
+  } else if (isFirstLaunch == true) {
+    return (routeName = 'Onboarding');
+  } else {
+    routeName = 'Login';
+  }
+
+  return (
+    <Stack.Navigator initialRouteName={routeName}>
+      <Stack.Screen
+        name="Onboarding"
+        component={OnboardingScreen}
+        options={{header: () => null}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{header: () => null}}
+      />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
---
+++ screens/SignupScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const SignupScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Signup Screen</Text>
+      <Button
+        title="click Here"
+        onPress={() => navigation.navigate('Signup')}
+      />
+    </View>
+  );
+};
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
